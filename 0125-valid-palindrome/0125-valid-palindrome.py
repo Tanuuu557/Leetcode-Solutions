762344c719ee407dc661b2eee24aed09ccb7bd26
Tanuuu557 <tanzcore1122@gmail.com>
2026-08-03 22:53:55 +0530

    Time: 142 ms (26.33%), Space: 13.1 MB (44.78%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s = s.lower()
+        result = ""
+
+        for char in s:
+            if char.isalnum():
+                result += char
+
+        left = 0
+        right = len(result) - 1
+
+        while left < right:
+            if result[left] != result[right]:
+                return False
+
+            left += 1
+            right -= 1
+        return True
+
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
