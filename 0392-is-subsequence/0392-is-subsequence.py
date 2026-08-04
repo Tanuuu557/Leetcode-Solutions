3e60db4f2fbc39c732beeae806ea01bffde64e29
Tanuuu557 <tanzcore1122@gmail.com>
2026-08-04 20:19:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (76.98%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        left = 0
+        right = 0
+
+        while left < len(s) and right < len(t):
+            if s[left] == t[right]:
+                left += 1
+            right += 1
+
+        return left == len(s)
+
+sol = Solution()
+print(sol.isSubsequence("abc", "ahbgdc"))
+        
